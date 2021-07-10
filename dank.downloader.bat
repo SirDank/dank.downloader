a8657d1f6861d1215ea6b2676414795491f06eee
SirDankenstien <52797753+SirDankenstien@users.noreply.github.com>
2021-07-10 18:16:56 +0530

    2.2

--- dank.downloader.bat
+++ dank.downloader.bat
@@ -1,39 +1,37 @@
 @echo off
 echo "========================================================================================="
-echo "   ___            __     ___                  __             __               ___   ___  "
-echo "  / _ \___ ____  / /__  / _ \___ _    _____  / /__  ___ ____/ /__ ____  _  __|_  | <  /  "
-echo " / // / _ `/ _ \/  '_/ / // / _ \ |/|/ / _ \/ / _ \/ _ `/ _  / -_) __/ | |/ / __/_ / /   "
-echo "/____/\_,_/_//_/_/\_(_)____/\___/__,__/_//_/_/\___/\_,_/\_,_/\__/_/    |___/____(_)_/    "
+echo "     ___            __     ___                  __             __          ___    ___    "
+echo "    / _ \___ ____  / /__  / _ \___ _    _____  / /__  ___ ____/ /__ ____  |_  |  |_  |   "
+echo "   / // / _ `/ _ \/  '_/ / // / _ \ |/|/ / _ \/ / _ \/ _ `/ _  / -_) __/ / __/_ / __/    "
+echo "  /____/\_,_/_//_/_/\_(_)____/\___/__,__/_//_/_/\___/\_,_/\_,_/\__/_/   /____(_)____/    "
 echo "                                                                                         "
 echo "========================================================================================="
 echo.
 
-echo Helps you download essential Windows 10, 64-Bit Programs :P ( well atleast for me )
 color 09
+color 0a
 color 0b
 color 0c
-color 0d
 color 0e
-color 0a
+color 0d
 color 09
+color 0a
 color 0b
 color 0c
-color 0d
 color 0e
-color 0a
+color 0d
 color 09
+color 0a
 color 0b
 color 0c
-color 0d
 color 0e
-color 0a
-echo.
+color 0d
 
 echo "==========< Essential Windows Stuff >=========="
 echo.
 set /P WA=Download Win10 Activator [Y/ENTER]: 
 set /P MCT=Download Win10 MediaCreationTool [Y/ENTER]: 
-set /P WDB=Open Github Windows10Debloater [Y/ENTER]: 
+set /P WDB=Open Github Windows10 Debloat Scripts [Y/ENTER]: 
 set /P NV=Open NVCleanstall { Nvidia GeForce Experience Alternative } [Y/ENTER]: 
 set /P RT=Open Realtek HD Audio Driver [Y/ENTER]: 
 echo.
@@ -42,21 +40,34 @@ echo.
 set /P ASC=Download IOBIT Advanced System Care [Y/ENTER]: 
 set /P DB=Download IOBIT Driver Booster [Y/ENTER]: 
 set /P SD=Download IOBIT Smart Defrag [Y/ENTER]: 
+set /P UN=Download IOBIT Uninstaller [Y/ENTER]: 
+set /P SU=Download IOBIT Software Updater [Y/ENTER]: 
 echo.
 echo "==========< Software >=========="
 echo.
 set /P WR=Open WinRAR 6 + Key [Y/ENTER]: 
+set /P NP=Open Notepad++ [Y/ENTER]: 
 set /P GC=Download Google Chrome [Y/ENTER]: 
 set /P FF=Download Firefox [Y/ENTER]: 
 set /P TR=Open Tor [Y/ENTER]: 
 set /P ST=Download Steam [Y/ENTER]: 
+set /P DISC=Download Discord [Y/ENTER]: 
 set /P MC=Download Minecraft [Y/ENTER]: 
+set /P BN=Download Battle.net [Y/ENTER]: 
 set /P TV=Download TeamViewer [Y/ENTER]: 
+set /P MS=Open MegaSync [Y/ENTER]: 
 set /P SB=Open Sandboxie [Y/ENTER]: 
+set /P VB=Open Virtualbox [Y/ENTER]: 
+set /P QB=Download QBittorrent [Y/ENTER]: 
+set /P VSC=Download Visual Studio Code [Y/ENTER]: 
+set /P GH=Download Github Desktop [Y/ENTER]: 
+set /P PY=Open Python [Y/ENTER]: 
+set /P JV=Open Java [Y/ENTER]: 
 echo.
 
-echo "==========< Downloading >=========="
+echo "==========< Starting [ Essential Windows Stuff ] >=========="
 echo.
+
 if /I [%WA%] EQU [Y] (
    echo Downloading Win10 Activator...
    powershell.exe -Command "Start-Process https://www.mediafire.com/file/2sdp6xh7ijahhd4/Win10-Activator.zip/file"
@@ -74,8 +85,8 @@ if /I [%MCT%] EQU [Y] (
 )
 
 if /I [%WDB%] EQU [Y] (
-   echo Opening Github Windows10Debloater...
-   powershell.exe -Command "Start-Process https://github.com/Sycnex/Windows10Debloater"
+   echo Opening Github Windows10 Debloat Scripts...
+   powershell.exe -Command "Start-Process https://github.com/search?o=desc&q=debloat&s=stars&type=Repositories"
    timeout /t 1 /nobreak > NUL
    echo Done!
    echo.
@@ -96,6 +107,9 @@ if /I [%RT%] EQU [Y] (
    echo Done!
    echo.
 )
+
+echo "==========< Starting [ Windows Optimization Software ] >=========="
+echo.
 
 if /I [%ASC%] EQU [Y] (
    echo Downloading IOBIT Advanced System Care...
@@ -121,9 +135,36 @@ if /I [%SD%] EQU [Y] (
    echo.
 )
 
+if /I [%UN%] EQU [Y] (
+   echo Downloading IOBIT Uninstaller...
+   powershell.exe -Command "Start-Process https://www.iobit.com/downloadcenter.php?product=advanced-uninstaller"
+   timeout /t 1 /nobreak > NUL
+   echo Done!
+   echo.
+)
+
+if /I [%SU%] EQU [Y] (
+   echo Downloading IOBIT Software Updater...
+   powershell.exe -Command "Start-Process https://www.iobit.com/downloadcenter.php?product=software-updater"
+   timeout /t 1 /nobreak > NUL
+   echo Done!
+   echo.
+)
+
+echo "==========< Starting [ Software ] >=========="
+echo.
+
 if /I [%WR%] EQU [Y] (
    echo Opening WinRAR...
-   powershell.exe -Command "Start-Process https://www.mediafire.com/file/picglyo9f87xvgo/WinRAR_6.zip/file"
+   powershell.exe -Command "Start-Process https://mega.nz/file/RHoHSCzR#rBA_3nbaFCFHbJZFMTol8zHGIkqqO-IjhyeoEU-UW1A"
+   timeout /t 1 /nobreak > NUL
+   echo Done!
+   echo.
+)
+
+if /I [%NP%] EQU [Y] (
+   echo Opening Notepad++...
+   powershell.exe -Command "Start-Process https://notepad-plus-plus.org/downloads/"
    timeout /t 1 /nobreak > NUL
    echo Done!
    echo.
@@ -146,7 +187,7 @@ if /I [%FF%] EQU [Y] (
 )
 
 if /I [%TR%] EQU [Y] (
-   echo Opening Tor Website...
+   echo Opening Tor...
    powershell.exe -Command "Start-Process https://www.torproject.org/download/"
    timeout /t 1 /nobreak > NUL
    echo Done!
@@ -161,6 +202,14 @@ if /I [%ST%] EQU [Y] (
    echo.
 )
 
+if /I [%DISC%] EQU [Y] (
+   echo Downloading Discord...
+   powershell.exe -Command "Start-Process https://discord.com/api/downloads/distributions/app/installers/latest?channel=stable&platform=win&arch=x86"
+   timeout /t 1 /nobreak > NUL
+   echo Done!
+   echo.
+)
+
 if /I [%MC%] EQU [Y] (
    echo Downloading Minecraft...
    powershell.exe -Command "Start-Process https://launcher.mojang.com/download/MinecraftInstaller.msi"
@@ -169,9 +218,25 @@ if /I [%MC%] EQU [Y] (
    echo.
 )
 
+if /I [%BN%] EQU [Y] (
+   echo Downloading Battle.net...
+   powershell.exe -Command "Start-Process https://www.blizzard.com/download/confirmation?platform=windows&locale=en_US&product=bnetdesk"
+   timeout /t 1 /nobreak > NUL
+   echo Done!
+   echo.
+)
+
 if /I [%TV%] EQU [Y] (
    echo Downloading TeamViewer...
-   powershell.exe -Command "Start-Process https://download.teamviewer.com/download/TeamViewer_Setup.exe"
+   powershell.exe -Command "Start-Process https://download.teamviewer.com/download/TeamViewer_Setup_x64.exe"
+   timeout /t 1 /nobreak > NUL
+   echo Done!
+   echo.
+)
+
+if /I [%MS%] EQU [Y] (
+   echo Opening MegaSync...
+   powershell.exe -Command "Start-Process https://mega.nz/sync"
    timeout /t 1 /nobreak > NUL
    echo Done!
    echo.
@@ -185,7 +250,55 @@ if /I [%SB%] EQU [Y] (
    echo.
 )
 
-echo "==========< Downloaded! >=========="
+if /I [%VB%] EQU [Y] (
+   echo Opening Virtualbox...
+   powershell.exe -Command "Start-Process https://www.virtualbox.org/wiki/Downloads"
+   timeout /t 1 /nobreak > NUL
+   echo Done!
+   echo.
+)
+
+if /I [%QB%] EQU [Y] (
+   echo Downloading QBittorrent...
+   powershell.exe -Command "Start-Process https://sourceforge.net/projects/qbittorrent/files/latest/download"
+   timeout /t 1 /nobreak > NUL
+   echo Done!
+   echo.
+)
+
+if /I [%VSC%] EQU [Y] (
+   echo Downloading Visual Studio Code...
+   powershell.exe -Command "Start-Process https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-user"
+   timeout /t 1 /nobreak > NUL
+   echo Done!
+   echo.
+)
+
+if /I [%GH%] EQU [Y] (
+   echo Downloading Github Desktop...
+   powershell.exe -Command "Start-Process https://central.github.com/deployments/desktop/desktop/latest/win32"
+   timeout /t 1 /nobreak > NUL
+   echo Done!
+   echo.
+)
+
+if /I [%PY%] EQU [Y] (
+   echo Opening Python...
+   powershell.exe -Command "Start-Process https://www.python.org/downloads/"
+   timeout /t 1 /nobreak > NUL
+   echo Done!
+   echo.
+)
+
+if /I [%JV%] EQU [Y] (
+   echo Opening Java...
+   powershell.exe -Command "Start-Process https://www.java.com/en/download/"
+   timeout /t 1 /nobreak > NUL
+   echo Done!
+   echo.
+)
+
+echo "==========< Complete >=========="
 echo.
 echo Downloaded Files located in your Downloads Folder xD
 echo.
@@ -214,23 +327,23 @@ echo " /_______  |__||__| /_______  (____  |___|  |__|_ \ "
 echo "         \/                 \/     \/     \/     \/ "
 echo.
 color 09
+color 0a
 color 0b
 color 0c
-color 0d
 color 0e
-color 0a
+color 0d
 color 09
+color 0a
 color 0b
 color 0c
-color 0d
 color 0e
-color 0a
+color 0d
 color 09
+color 0a
 color 0b
 color 0c
-color 0d
 color 0e
-color 0a
+color 0d
 echo.
 echo "==========< 69 >=========="
 powershell.exe -Command "Start-Process https://allmylinks.com/sir-dankenstein"
